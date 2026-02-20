db797c07e49b5942041bae13e6cc4f26e8cb3fdf
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-02-20 22:27:56 +0300

    Create source2.cpp

---
+++ source2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int size = 10;
+    int arr[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int min = arr[0];
+    int max = arr[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    std::cout << "Массив: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << '\n' << "Минимальный элемент: " << min ;
+    std::cout << '\n' << "Максимальный элемент: " <<max;
+    return 0;
+}
