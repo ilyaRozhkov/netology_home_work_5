9063a92464f689a4fcc0af26b83eb82d5b4ae43c
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-02-20 22:40:27 +0300

    Update source2.cpp

--- source2.cpp
+++ source2.cpp
@@ -12,18 +12,17 @@ int main() {
     int arr[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int min = arr[0];
     int max = arr[0];
-    for (int i = 1; i < size; i++)
+    
+    std::cout << "Массив: ";
+    for (int i = 0; i < size; i++)
     {
+                std::cout << arr[i] << ' ';
         if (min > arr[i]) {
             min = arr[i];
         }
         if (max < arr[i]) {
             max = arr[i];
         }
-    }
-    std::cout << "Массив: ";
-    for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << ' ';
     }
     std::cout << '\n' << "Минимальный элемент: " << min ;
     std::cout << '\n' << "Максимальный элемент: " <<max;
